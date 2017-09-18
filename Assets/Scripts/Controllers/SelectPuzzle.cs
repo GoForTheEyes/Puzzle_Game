47110eb8bbdbabba84fd0c9c7117e4c9c929612d
GoForTheEyes <brabandere@gmail.com>
2017-09-17 22:11:02 -0400

    Day1
    First day of dev

---
+++ Assets/Scripts/Controllers/SelectPuzzle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectPuzzle : MonoBehaviour {
+
+    public GameObject selectPuzzleMenuPanel, puzzleLevelSelectPanel;
+    public Animator selectPuzzleMenuAnim, puzzleLevelSelectAnim;
+
+    private string selectedPuzzle;
+
+    public void SelectedPuzzle()
+    {
+        selectedPuzzle = EventSystem.current.currentSelectedGameObject.name; //Gives selected object name
+        StartCoroutine(ShowPuzzleLevelSelectMenu());
+    }
+
+    IEnumerator ShowPuzzleLevelSelectMenu()
+    {
+        //Changes from the select category of puzzle to the level of selected puzzle view
+        puzzleLevelSelectPanel.SetActive(true);
+        selectPuzzleMenuAnim.Play("SlideOut");
+        puzzleLevelSelectAnim.Play("SlideIn");
+        yield return new WaitForSeconds(1f);
+        selectPuzzleMenuPanel.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/Controllers/SettingsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsController : MonoBehaviour {
+
+    public GameObject settingsPanel;
+    public Animator settingsPanelAnim;
+
+    public void OpenSettingsPanel()
+    {
+        settingsPanel.SetActive(true);
+        settingsPanelAnim.Play("SlideIn");
+    }
+
+    public void CloseSettingsPanel()
+    {
+        StartCoroutine(CloseSettings());
+    }
+
+    IEnumerator CloseSettings()
+    {
+        settingsPanelAnim.Play("SlideOut");
+        yield return new WaitForSeconds(1f);
+        settingsPanel.SetActive(false);
+    }
+
+
+}
